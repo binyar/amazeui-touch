e649b642e3d6d9c1109dbc56da16170e772add60
fanmd <binyar@qq.com>
2016-09-29 17:38:24 +0800

    badge-->reverse

--- js/Badge.js
+++ js/Badge.js
@@ -17,6 +17,7 @@ const Badge = React.createClass({
     // radius: PropTypes.bool,
     rounded: PropTypes.bool,
   },
+
   getDefaultProps() {
     return {
       classPrefix: 'badge',
@@ -30,10 +31,9 @@ const Badge = React.createClass({
       component: Component,
       className,
       href,
-  ...
-    props
-  }
-    = this.props;
+      ...props
+    } = this.props;
+
     delete props.classPrefix;
     delete props.amStyle;
     delete props.rounded;
@@ -41,18 +41,13 @@ const Badge = React.createClass({
     Component = href ? 'a' : Component;
 
     return (
-      < Component
-    {...
-      props
-    }
-    className = {cx(classSet, className)}
+      <Component
+        {...props}
+        className={cx(classSet, className)}
       >
-      {this.props.children
-  }
-  </
-    Component >
-  )
-    ;
+        {this.props.children}
+      </Component>
+    );
   }
 });
 
